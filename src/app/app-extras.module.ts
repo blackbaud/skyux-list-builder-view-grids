0c9e20dde3ad6266e59c36caa9cc159f328dbd7a
Alex Kingman <alex.kingman@blackbaud.com>
2018-10-23 15:20:51 -0400

    added visual tests

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -5,14 +5,32 @@ import {
   SkyListViewGridModule
 } from './public/modules/list-view-grid/list-view-grid.module';
 
+import {
+  SkyGridModule
+} from '@skyux/grids';
+import {
+  SkyListModule
+} from '@skyux/list-builder';
+
+import {
+  ListStateDispatcher,
+  ListState
+} from '@skyux/list-builder/modules/list/state';
+
 @NgModule({
   imports: [
+    SkyGridModule,
+    SkyListModule,
     SkyListViewGridModule
   ],
   exports: [
+    SkyGridModule,
+    SkyListModule,
     SkyListViewGridModule
   ],
-  providers: [],
-  entryComponents: []
+  providers: [
+    ListState,
+    ListStateDispatcher
+  ]
 })
 export class AppExtrasModule { }
