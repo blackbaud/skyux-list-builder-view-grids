6a71c720351c22362fc6c2dd4b1e1d7c89281d08
Alex <alex.kingman@blackbaud.com>
2019-01-08 14:57:25 -0500

    Added rowHighlightedId (#14)
    * Fixed issues with unit tests
    
    * added rowHighlightedId input
    
    * fixed merge
    
    * removed unnecessary demo stuff
    
    * updated visual tests
    
    * fixed button method

--- e2e/list-view-grid.e2e-spec.ts
+++ e2e/list-view-grid.e2e-spec.ts
@@ -13,14 +13,18 @@ describe('List view grid', () => {
     SkyHostBrowser.get('/visual/list-view-grid');
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#screenshot-list-view-grid');
-    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done);
+    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done, {
+      screenshotName: 'list-view-grid-lg'
+    });
   });
 
   it('should match the previous screenshot (screen: xs)', (done) => {
     SkyHostBrowser.get('/visual/list-view-grid');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#screenshot-list-view-grid');
-    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done);
+    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done, {
+      screenshotName: 'list-view-grid-xs'
+    });
   });
 
   it('should match the previous screenshot with column chooser open', (done) => {
@@ -30,7 +34,9 @@ describe('List view grid', () => {
 
     element(by.css('[sky-cmp-id="column-chooser"] button')).click();
 
-    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done);
+    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done, {
+      screenshotName: 'list-view-grid-column-chooser-lg'
+    });
   });
 
   it('should match the previous screenshot with column chooser open (screen: xs)', (done) => {
@@ -40,6 +46,32 @@ describe('List view grid', () => {
 
     element(by.css('[sky-cmp-id="column-chooser"] button')).click();
 
-    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done);
+    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done, {
+      screenshotName: 'list-view-grid-column-chooser-xs'
+    });
+  });
+
+  it('should match the previous screenshot with a highlighted row', (done) => {
+    SkyHostBrowser.get('/visual/list-view-grid');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-list-view-grid');
+
+    element(by.css('#highlight-row-button')).click();
+
+    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done, {
+      screenshotName: 'list-view-grid-highlighted-row-lg'
+    });
+  });
+
+  it('should match the previous screenshot with a highlighted row (screen: xs)', (done) => {
+    SkyHostBrowser.get('/visual/list-view-grid');
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    SkyHostBrowser.scrollTo('#screenshot-list-view-grid');
+
+    element(by.css('#highlight-row-button')).click();
+
+    expect('#screenshot-list-view-grid').toMatchBaselineScreenshot(done, {
+      screenshotName: 'list-view-grid-highlighted-row-xs'
+    });
   });
 });
