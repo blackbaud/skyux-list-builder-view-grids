8a6e0a7a33b80cabbef4b5be49f9e7b1bea061ef
Alex Kingman <alex.kingman@blackbaud.me>
2021-09-15 08:38:00 -0400

    visual

--- projects/list-builder-view-grids-showcase/src/app/app-routing.module.ts
+++ projects/list-builder-view-grids-showcase/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListViewGridTestComponent } from './visual/list-view-grid/list-view-grid-visual.component';
+import { VisualComponent } from './visual/visual.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: VisualComponent
+  },
+  {
+    path: 'visual/list-view-grid',
+    component: ListViewGridTestComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
