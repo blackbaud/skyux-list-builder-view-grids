8f0adbeff9d98c828e4643cbf1aefa848e2d0178
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 13:39:31 -0500

    Updated README.md to point contributors to the monorepo [ci skip]

--- projects/list-builder-view-grids/src/modules/list-view-grid/state/grid-state-action.type.ts
+++
@@ -1,6 +0,0 @@
-import { ListViewGridColumnsLoadAction } from './columns/load.action';
-
-/**
- * @internal
- */
-export type GridStateAction = ListViewGridColumnsLoadAction;
--- projects/list-builder-view-grids/testing/src/public-api.ts
+++
@@ -1,4 +0,0 @@
-export * from './list-view-grid-fixture';
-export * from './list-view-grid-fixture-cell';
-export * from './list-view-grid-fixture-header';
-export * from './list-view-grid-fixture-row';
